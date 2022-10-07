f21720c9ce2679da7697e35886c965158b22834f
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-10-07 19:14:04 +0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/index';
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Navigate to="/main" replace={true} />}/>
+        <Route path='/main' element={<Home />}/>
+      </Routes>
+      
     </div>
   );
 }
